2a94d33b271baa2167b3b1e9e5ae755faac56c21
antony <djalma.rodrigues@luizalabs.com>
2024-02-21 23:47:54 -0300

    feat: add types in country card

--- src/ui/components/IconPressable/styles.ts
+++ src/ui/components/IconPressable/styles.ts
@@ -2,6 +2,8 @@ import { createStyleSheet } from 'react-native-unistyles';
 
 export const stylesheet = createStyleSheet((theme) => ({
   iconPressable: {
+    flexDirection: 'row',
+    gap: theme.spacing.xsm,
     padding: theme.spacing.xsm,
     borderRadius: theme.spacing.xsm,
   },
